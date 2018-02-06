3e0cfc881e73d8321558a02a6c123a2a7ac62738
Sam White <samuel.j.white@gmail.com>
2018-02-06 13:16:59 -0800

    very brief outline for data handling

--- scripts/pH_calibration_check.R
+++ scripts/pH_calibration_check.R
@@ -1,11 +1,16 @@
-install.packages("tidyverse")
-install.packages("whoami")
+#install.packages("tidyverse")
+#install.packages("whoami")
+
+# Load necessary libraries. 
 library(whoami)
 library(tidyverse)
 
-# Extracts current Windows username
+# Extracts current Windows username.
 win_user <- username()
 
 # Set working directory to titrator calibration data Git repo.
 setwd(file.path("c:/Users/", win_user, "/gitrepos/RobertsLab/titrator/data/cal_data/"))
 
+# Set up list/array comparisons of files in folder.
+
+# Operate on newly added file.
