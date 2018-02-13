aae79c8bb4db3b95b16dad6c9fb8512b3e7bc404
Sam White <samuel.j.white@gmail.com>
2018-02-12 16:06:37 -0800

    add CRM constants; add csv file encoding

--- scripts/parsing_TA_output.R
+++ scripts/parsing_TA_output.R
@@ -1,15 +1,24 @@
 library(seacarb)
 library(XLConnect)
 library(tidyverse)
+library(whoami)
 
-# Set working directory.
-# Will need to be altered based on user and computing operating system.
-setwd("c:/Users/sam/gitrepos/RobertsLab/titrator/data/titration_data/")
+# Extracts current Windows username.
+win_user <- username()
+
+### Set working directory.
+# Set working directory to titrator calibration data Git repo.
+setwd(file.path("c:/Users/", win_user, "/gitrepos/RobertsLab/titrator/data/titration_data"))
 
 # Acid titrant constants
 #Batch A10
-A10_density <- 1.02449 #g/cm^3
-A10_concentration <- 0.100215 #mol/kg
+A10_density <- 1.02449 # g/cm^3
+A10_concentration <- 0.100215 # mol/kg
+
+# CRM constants
+#Batch 168
+CRM168_TA <- 2207.62 # umol/kg
+CRM168_salinity <- 33.481 # PSU (~g/kg)
 
 
 #Enter voltage cutoffs
@@ -22,7 +31,7 @@ pH3.5 <- 200
 mol_to_umol <- 1000000
 
 
-data1 <- read.csv(file = "2018-02-09T11_53_39_TA_titration_T215.csv", header = FALSE, stringsAsFactors = FALSE)
+data1 <- read.csv(file = "2018-02-09T14_50_38_TA_titration_T232.csv", header = FALSE, stringsAsFactors = FALSE, fileEncoding="UTF-8-BOM")
 
 # Pulls total sample number from Row 2, Col. 2, position 11.
 # Converts from string to number.
@@ -39,7 +48,7 @@ total_samples <- as.numeric(data1[2,2] %>% substr(11,11))
 weights_with_units <- data1[grep("^Sample size", data1$V1), 2]
 
 # Then determines the string length by converting to characters and counting the characters.
-# Subtracts two from character length to account for <space>g at end of entry.
+# Subtracts two from character length to account for "<space>g" at end of entry.
 weight_char_counts <- data1[grep("^Sample size", data1$V1), 2] %>% 
   nchar() %>% 
   as.numeric() - 2
