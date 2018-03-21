e8522d6b577f6b668c54855025f3a5e4c17e5eae
Sam White <samuel.j.white@gmail.com>
2018-03-21 09:55:38 -0700

    improve commenting; add filename parsing

--- scripts/pH_calibration_check.R
+++ scripts/pH_calibration_check.R
@@ -1,23 +1,12 @@
-#
-# This script is currently under development.
-# It is non-functional.
-# 20180206 SJW
-#
+### Load filename for downstream use
+cal_data_file <- '2018-03-16T10_19_31_pH_calibration_7_4_10_T303.csv'
 
+# Extract calibration date.
+# Uses substring to parse out original data format, followed by gsub to remove dashes.
+cal_date <- gsub('-', '', substr(cal_data_file, 1, 10))
 
-#################
-#install.packages("tidyverse")
-#install.packages("whoami")
-
-# Load necessary libraries. 
-library(tidyverse)
-
-
-# Set up list/array comparisons of files in folder.
-
-# Operate on newly added file.
-
-cal_data_file <- 'data/cal_data/2018-03-01T09_08_38_pH_calibration_7_4_10_T275.csv'
+# Extract LabX task ID value from filename.
+cal_task <- substr(cal_data_file, 43, as.numeric(nchar(cal_data_file) - 4))
 
 
 ### Read data in as csv table that handles issue of having more columns in bottom portion of file than in top portion.
@@ -25,23 +14,29 @@ cal_data_file <- 'data/cal_data/2018-03-01T09_08_38_pH_calibration_7_4_10_T275.c
 # Sets number of columns and assigns column names (V#) based on total number of fields detected in the file.
 cal_data <- read.table(cal_data_file, header = FALSE, stringsAsFactors = FALSE, fileEncoding="UTF-8-BOM", sep = ",", col.names = paste0("V",seq_len(max(count.fields(cal_data_file, sep = ',')) - 1)), fill = TRUE)
 
-# Set constants
+### Set constants
+## headers
+# t = time (s)
+# E = voltage (mV)
+# T = temperature (C)
+pH_data_headers <- c("t", "E", "T")
+
 pH_buffers <-c(4, 7, 10) #Vector of pH buffers used for calibration.
 pH3.5_3.0 <-c(3.5, 3.0) #Vector of titration endpoint pH values
 
 
-# Calculate mean voltages (E) for each pH buffer; this data is in column 2
-mean_E_pH4.0 <- mean(as.numeric(cal_data[202:231,2])) #Rows 130 - 159
-mean_E_pH7.0 <- mean(as.numeric(cal_data[169:198,2])) #Rows 97 - 126
-mean_E_pH10.0 <- mean(as.numeric(cal_data[235:264,2])) #Rows 163 - 192
+### Calculate mean voltages (E) for each pH buffer; this data is in column 2
+mean_E_pH4.0 <- mean(as.numeric(cal_data[202:231,2]))
+mean_E_pH7.0 <- mean(as.numeric(cal_data[169:198,2]))
+mean_E_pH10.0 <- mean(as.numeric(cal_data[235:264,2]))
 
-
-# Determine y intercept and slope of best fit line
-buffers_E <-c(mean_E_pH4.0, mean_E_pH7.0, mean_E_pH10.0)
+### Determine y intercept and slope of best fit line
+# Calculate mean voltages (E) of each buffer
+buffers_mean_E <-c(mean_E_pH4.0, mean_E_pH7.0, mean_E_pH10.0)
 
 # Run linear model of voltages and corresponding pH buffer
-model<-lm(buffers_E ~ pH_buffers)
+model<-lm(buffers_mean_E ~ pH_buffers)
 
 # Use coef of model to extract the best fit slope ((model)[2]) and y intercept ((model)[1]).
-# Use those values (voltages in mV = E) to determine voltages for pH3.0 & pH3.5
-E_pH3.0_3.5 <- coef(model)[2]*pH3.5_3.0+coef(model)[1]
+# Use those values (voltages in mV = E) to determine voltages for pH3.5 & pH3.0
+E_pH3.5_3.0 <- coef(model)[2]*pH3.5_3.0+coef(model)[1]
